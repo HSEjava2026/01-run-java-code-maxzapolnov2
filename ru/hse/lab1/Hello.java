b1c609ea3671f2f3232fdbe0f5456dde5ed96571
maxzapolnov2 <maxzapolnov2@gmail.com>
2026-02-08 22:38:22 +0300

    написал комментарии джавадок

--- ru/hse/lab1/Hello.java
+++ ru/hse/lab1/Hello.java
@@ -1,5 +1,9 @@
 package ru.hse.lab1;
-
+/**
+* @author Запольнов Максим Александрович
+* @version 1.0
+* @since 2026
+*/
 public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello World!");
--- ru/hse/lab1/Main.java
+++ ru/hse/lab1/Main.java
@@ -1,7 +1,11 @@
 package ru.hse.lab1;
 
 import ru.hse.lab1.second.Person;
-
+/**
+* @author Запольнов Максим Александрович
+* @version 1.0
+* @since 2026
+*/
 public class Main {
     public static void main(String[] args) {
         Person person = new Person("Анатолий", 43);
